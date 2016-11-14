962f566d09c5274c69c86ec24d69d8c5d60ef055
Juan Fraire <groteck@gmail.com>
2016-11-14 14:55:51 +0000

    Add stop hour to stop the bot out of workin hours

--- lib/lita/handlers/keepalive.rb
+++ lib/lita/handlers/keepalive.rb
@@ -3,15 +3,28 @@ module Lita
     class Keepalive < Handler
       config :url, required: true, type: String
       config :minutes, required: true, type: Integer
+      config :end_hour, required: false, type: Integer
 
-      http.get "/ping" do |request, response|
+      http.get '/ping' do |request, response|
         response.body << "pong"
+      end
+
+      def workin_hours?
+        !config.end_time.nil? &&
+          Time.now.strftime('%H').to_i > config.end_hour
       end
 
       on(:loaded) do
         log.info "Starting Keepalive to #{config.url}/ping"
         every(config.minutes * 60) do
-          log.info "Keepalive ping..."
+          log.info 'Trying to run Keepalive!!!'
+          log.info workin_hours?
+
+          if workin_hours?
+            log.info 'Out of working hours!!!'
+            next
+          end
+          log.info 'Keepalive ping...'
           http.get "#{config.url}/ping"
         end
       end
